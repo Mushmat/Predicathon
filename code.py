f7d35cb3c470bb750023e358d33f443ffef2b361
Mushmat <edu.chirayu2005@gmail.com>
2025-01-27 21:43:50 +0530

    Changed the file name

--- code.py
+++
@@ -1,6 +0,0 @@
-import os
-import json
-import cv2
-import numpy as np
-import matplotlib.pyplot as ply
-
---
+++ model.py
@@ -0,0 +1,11 @@
+import os
+import json
+import cv2
+import numpy as np
+import matplotlib.pyplot as ply
+
+# Paths
+train_images_path = ""
+train_json_path = ""
+test_images_path = ""
+test_json_path = ""
