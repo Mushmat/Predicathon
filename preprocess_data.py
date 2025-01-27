3cdbd0c6925a7785cbc8bd1d67944bbb741a5041
Mushmat <edu.chirayu2005@gmail.com>
2025-01-27 22:03:36 +0530

    function to resize and normalize the images

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,14 @@
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
+import cv2
+import numpy as np
+
 
 # Preprocessing (Resize and Normalize)
+def preprocess_images(images, size=(224,224)):
+    resized_images = []
+    for img in images:
+        resized_img = cv2.resize(img,size) / 255.0
+        resized_images.append(resized_img)
+    return np.array(resized_images)
 
